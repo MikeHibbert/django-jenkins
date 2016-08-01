fc9895ccd338f75d5f9d89f432ff4c921829150a
Mike Hibbert <hibbert.michael@gmail.com>
2016-08-01 14:14:15 +0100

    added support for django >= 1.9

--- django_jenkins/management/commands/__init__.py
+++ django_jenkins/management/commands/__init__.py
@@ -5,7 +5,14 @@ import sys
 from optparse import make_option, OptionGroup
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from django.utils.importlib import import_module
+
+try:
+    # Django versions >= 1.9
+    from django.utils.module_loading import import_module
+except ImportError:
+    # Django versions < 1.9
+    from django.utils.importlib import import_module
+    
 from django_jenkins import signals
 from django_jenkins.runner import CITestSuiteRunner
 
